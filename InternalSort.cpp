23096f8c87a0a653ca7d7797d9f15645600ab7aa
Hector <myourys@gmail.com>
2013-03-27 03:15:53 +0800

    Add KMP.cpp

--- InternalSort.cpp
+++ InternalSort.cpp
@@ -153,33 +153,103 @@ void BInsertSort()
  * 希尔排序
  * O(N*(logN)2)
  * 希尔排序是插入排序的一种。
+ * 如此例第一次分为5组,上下为一组,然后每组排序
+ * 8,1,3,0,9           得到: 8,2,4,6,9
+ * 7,2,4,6,5                 7,1,3,0,5
+ * 然后分为5/2=2组,每组排列...
+ * 直至分为1组就是有序了
  */
 void ShellSord()
 {
-    int i;
+    int i,j;
     for(int gap =n/2;gap>0;gap /=2) //每次折半
-        for(i=gap;i<n;j++)
+        for(i=gap;i<n;i++) //从第二行数据开始,直接插入排序
         {
-
+            for(j=i-gap;j>=0 && s[j+gap]>s[j];j-=gap) //与之前一列数据比较,此处参考插入排序
+                swap(s[j],s[j+gap]);
         }
 }
+
+/*
+ * 基数排序
+ * O (nlog(r)m)，其中r为所采取的基数，而m为堆数
+ * 在某些时候，基数排序法的效率高于其它的比较性排序法
+ * 即:先按个位排序,然后按十位排序...
+ */
+
+void RadixSord(int *m)
+{
+    int w = 6,j,k,r=1; //最大6位数
+    for(int i = 1;i<=w;i++)
+    {
+        for(j=1;j<n;j++) //插入排序
+            for(k=j-1;k>=0 && m[k+1]/r % 10 > m[k]/r %10;k--)
+                swap(m[k],m[k+1]);
+
+        r*=10;
+    }
+}
+
+
 
 /*
  * 堆排序
  */
 
 /*
- * 基数排序
+ * 归并排序
+ * O(N*logN)
+ * 将两个有效数列合并成一个,因此可以考虑分成两个1组,2组合并成1组,直至整个数列完成
  */
+void Merge(int a[], int b[], int low, int mid, int high)  //将A序列的两组(low-mid,mid+1-high),合并到同大小的b中
+{
+    int k = low;
+    int begin1 = low;
+    int end1 = mid;
+    int begin2 = mid + 1;
+    int end2 = high;
+    while(k <= high )
+    {
+        if(begin1 > end1) //第1组完成,将第二组附加上去
+            b[k++] = a[begin2++];
+        else if(begin2 > end2)
+            b[k++] = a[begin1++];
+        else
+        {
+            if(a[begin1] <= a[begin2])
+                b[k++] = a[begin1++];
+            else
+                b[k++] = a[begin2++];
+        }
+    }
+}
+
+void PassMergePass(int a[], int b[], int seg, int size)
+{
+
+}
+void MergeSord()
+{
+    int *temp=new int[n];
+
+}
+
 
 int main()
 {
     //BubbingSord();
    // FastSord(0,n-1);
     //BInsertSort();
-    //ShellSord(n/2);
+    ShellSord();
     for(int i =0;i<n;i++)
         cout<<s[i];
+
+    cout<<endl;
+
+    int m[] ={10223,22,55,922,531,7,0,999999,888,347443};
+    RadixSord(m);
+    for(int i =0;i<n;i++)
+        cout<<m[i]<<' ';
     return 0;
 }
 
---
+++ KMP.cpp
@@ -0,0 +1,108 @@
+/*=============================================================================
+#     FileName: KMP.cpp
+#         Desc: 字符串搜索算法
+#       Author: Hector
+#        Email: myourys@gmail.com
+#     HomePage: http://www.yiwuye.com
+#      Version: 0.0.1
+#   LastChange: 2013-03-27 01:49:43
+#      History:
+=============================================================================*/
+/* 一般字符串搜索算法时间复杂度O(m*n) m,n分别为原串和子串的长度
+ * KMP 时间复杂度 为 O(m+n)
+ * 本文参考http://billhoo.blog.51cto.com/2337751/411486
+ */
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+/*
+ * 一般字符串查找函数,查找成功,返回在原串中的位置,否则返回-1
+ * S[]原串,T[]待搜索的目标子串,pos 搜索起始点
+ */
+
+int StrSearch(const char S[],const char T[], int pos=0 )
+{
+    int i = pos, j = 0;
+    //i 原串搜索位置,每次+1
+    //j 子串被匹配长度,如果j等于子串长度,则匹配成功
+    while ( S[i+j] != '\0'&& T[j] != '\0')
+    if ( S[i+j] == T[j] )
+        j ++; // 继续比较后一字符
+    else
+    {
+        i ++; j = 0; // 重新开始新的一轮匹配
+    }
+
+    if ( T[j] == '\0')
+        return i; // 匹配成功   返回下标
+    else
+        return -1; // 串S中(第pos个字符起)不存在和串T相同的子串
+} 
+
+/*
+ * KM - 获取子串的模式值数组(前后缀串)
+ * 即:即是前缀串又是后缀串的子串长度
+ * 如:abcabca 为abca,即是4
+ * KMP即是如果匹配了这7个字符,则跳3个字符(直接原串和T[4]比较),如下:
+ * abcabcax
+ *    abcabca
+ */
+void GetNext(const char *T,int next[])
+{
+     register int iLen = 0;    // Length of T
+     while( '\0' != T[iLen] )
+         iLen++;
+
+     int LOLP = 0;     // Lenth of longest prefix,前后串
+     next[1] = 0;
+     for( int NOCM=2; NOCM<iLen+1; NOCM++ )     // NOCM represent the number of characters matched
+     {
+         //如:abaaba+b ,b!=T[3]
+         //则判断 此前后串的长度3的前后串,
+         //即aba的前后串(长度为1,为a),此时a后面为b,和新加的字符相同,跳出循环
+         //得到新的字符串的前后串为ab
+         while( LOLP>0 && (T[LOLP] != T[NOCM-1]) )
+            LOLP = next[LOLP];
+
+         if( T[LOLP] == T[NOCM-1] ) //如果新的字符也满足前后串,则前后串长度加1,如abcabc+a, a = T[3],前后串长度变为4
+             LOLP++;
+         next[NOCM] = LOLP;
+     }
+}
+
+/*
+ * KMP算法
+ */
+
+int KMPSearch(const char S[],const char T[])
+{
+    int SLen,TLen;
+    while( '\0' != S[SLen] )
+        SLen++;
+    while( '\0' != T[TLen] )
+        TLen++;
+
+    int *next = new int[TLen];
+    GetNext(T,next);
+    int NOCM = 0;     // Number of characters matched       
+
+    cout<<next[8]<<endl;
+    for(int i=0;i<SLen-TLen+1;i++)
+    {
+        while(NOCM>0 && S[i]!=T[NOCM])
+            NOCM = next[NOCM];
+
+        if(S[i]==T[NOCM])
+            if(++NOCM == TLen)
+                return i-TLen+1;
+    }
+    if(NOCM!=TLen) return -1;
+}
+int main()
+{
+    cout<<StrSearch("abccabccabca","cabc")<<endl;
+    cout<<KMPSearch("abccabccabca","cabc")<<endl;
+
+}
+
