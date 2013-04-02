b79a479fc85d6d39a0d99519c4349e5acee09c07
Hector <myourys@gmail.com>
2013-04-02 17:36:29 +0800

    Update InternalSort.cpp

--- InternalSort.cpp
+++ InternalSort.cpp
@@ -11,6 +11,7 @@
 
 #include<iostream>
 #include <stdlib.h>
+#include <ctime> 
 using namespace std;
 
 void swap(int &a,int &b)
@@ -66,7 +67,7 @@ void SimpleSelectSort(int s[],int n)
  * 以开头第一个元素为中间点,大的放在前面,小的放在后边,递归排序
  * 分别从后往前搜索,将大数放前面,然后从前往后搜索,将小的放在后边
  */
-void FastSort(int s[],int begin,int end)
+void QuickSort(int s[],int begin,int end)
 {
     if(begin>=end) return; //重合点
     // a,b 为前后搜索的定位点,e为中间点
@@ -92,8 +93,8 @@ void FastSort(int s[],int begin,int end)
             }
         }
     }
-    FastSort(s,begin,e-1);
-    FastSort(s,e+1,end);
+    QuickSort(s,begin,e-1);
+    QuickSort(s,e+1,end);
 }
 
 /*
@@ -240,7 +241,7 @@ void SubMerge(int a[], int b[], int ibegin, int mid,int iend)
 
    //同时保持a也有序
    for(i=ibegin;i<=iend;i++)
-       a[i]=b[i];
+	   a[i]=b[i];
 }
 
 /*
@@ -267,17 +268,73 @@ void MergeSort(int a[],int size)
     delete []temp;
 }
 
+/*
+ * 堆排序 [采用数组排序]
+ * O(N * logN)
+ * 1．父结点的键值总是大于或等于（小于或等于）任何一个子节点的键值。
+ * 2．每个结点的左子树和右子树都是一个二叉堆（都是最大堆或最小堆）
+ * i结点的父结点下标就为(i – 1) / 2，孩子2 * i + 1和2 * i + 2
+ * 由于每次重新恢复堆的时间复杂度为O(logN)，共N - 1次重新恢复堆操作，再加上前面建立堆时N / 2次向下调整，
+ * 每次调整时间复杂度也为O(logN)。二次操作时间相加还是O(N * logN)。故堆排序的时间复杂度为O(N * logN)
+ */
+void MiniHeap(int a[],int n); //堆话数组 ->建立最小堆
+void MinHeapFixdown(int a[],int i,int n); //调整堆元素
+void HeapSort(int a[],int n)
+{
+	int* t = new int[n];
+	for(int i=0;i<n;i++)
+		t[i]=a[i];
+	MiniHeap(t,n);//建立堆
+	for(int i=0;i<n;i++)
+	{
+		a[i] = t[0];
+		t[0] = t[n-i-1];//删除堆顶，并调整堆,使堆顶是最小元素
+		MinHeapFixdown(t,0,n-i);
+	}
+	delete []t;
+}
+
+void MiniHeap(int a[],int n) //堆话数组 ->建立最小堆
+{
+	//i 从最后一个不是叶子节点的位置开始调整堆
+	for(int i= n/2 - 1;i>=0;i--)
+		MinHeapFixdown(a,i,n);
+}
+
+void MinHeapFixdown(int a[],int i,int n) //大数下沉，自下而上
+{
+	int child = 2*i+1; //左孩子
+	if(child>=n)//无左右孩子
+		return;
+	if(child+1<n && a[child+1]<a[child]) //找到左右孩子最小的
+		child++;
+	if(a[child]>=a[i]) //如果本身就是有序的
+		return;
+	swap(a[i],a[child]);
+	MinHeapFixdown(a,child,n);//孩子继续下沉
+}
+
+/*
+ * 桶排序
+ */
+void BucketSort(int a[],int n)
+{
+	int b[1000][n] = new int; //1千个桶
+}
 
 int main()
 {
 	int Flag = 0;
-	while(1)
+	while(Flag <= 9)
 	{
 		Flag++;
-		if(Flag >7)
-			break;
-		int n = 10;
-		int s[10]={8,1,3,0,9,7,2,4,6,5};
+		int n = 10000;
+		int* s =new int[n];
+		for(int i=0;i<n;i++)
+			s[i] = rand() * rand()+1; //注rand()产生的数在0到65536之间
+		srand(time(NULL));
+		clock_t ibegin, iend; 
+		ibegin = clock();  
 		switch(Flag)
 		{
 		case 1:
@@ -287,8 +344,8 @@ int main()
 			cout<<"\n简单选择排序【SimpleSelectSort】："<<endl;
 			SimpleSelectSort(s,n);break;
 		case 3:
-			cout<<"\n快速排序【FastSort】："<<endl;
-			FastSort(s,0,n-1);break;
+			cout<<"\n快速排序【QuickSort】："<<endl;
+			QuickSort(s,0,n-1);break;
 		case 4:
 			cout<<"\n插入排序【InsertSort】："<<endl;
 			InsertSort(s,n);break;
@@ -301,17 +358,19 @@ int main()
 		case 7:
 			cout<<"\n归并排序【MergeSort】："<<endl;
 			MergeSort(s,n);break;
+		case 8:
+			cout<<"\n堆排序【MiniHeap】："<<endl;
+			HeapSort(s,n);break;
+		case 9:
+			cout<<"\n基数排序【RadixSort】："<<endl;
+			RadixSort(s,n);break;
 		}
-		
-		for(int i =0;i<n;i++)
-			cout<<s[i]<<"  ";
+		iend = clock();
+		if(iend - ibegin>0.1)
+			cout<<"时间（毫秒）："<<iend - ibegin<<endl;
+		/*for(int i =0;i<n;i++)
+			cout<<s[i]<<"  ";*/
 	}
-
-    int m[10] ={10223,22,55,922,531,7,0,999999,888,347443};
-	cout<<"\n基数排序【RadixSort】："<<endl;
-    RadixSort(m,10);
-    for(int i =0;i<10;i++)
-        cout<<m[i]<<"  ";
 
 	cout<<endl;
     return 0;
